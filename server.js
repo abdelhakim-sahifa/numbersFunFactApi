501e455e10067bbff7853ced74970ebc71d97c2a
abdelhakim sahifa <abdelhakim.sahifa@gmail.com>
2025-03-12 02:07:43 +0000

    cors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from "express";
+import cors from "cors";  // Import the CORS package
 import { getFact } from "./numFacts.js";
 
-
-
 const app = express();
-const PORT =  3000;
+const PORT = 3000;
+
+// Use CORS middleware to allow all origins
+app.use(cors());
 
 app.get("/fact/:number", async (req, res) => {
     const { number } = req.params;
-    console.log(number)
-    
+    console.log(number);
+
     // Validate number
     if (isNaN(number)) {
         return res.status(400).json({ error: "Invalid number parameter" });
